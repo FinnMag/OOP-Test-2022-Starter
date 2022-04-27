555029c78003f0f28dd0a2676f49afec4503f882
FinnMag <finnmaguire2002@gmail.com>
2022-04-27 18:50:35 +0100

    final

---
+++ java/src/ie/tudublin/Nematode.java
@@ -0,0 +1,82 @@
+package ie.tudublin;
+
+import processing.core.PApplet;
+import processing.data.TableRow;
+
+public class Nematode extends PApplet {
+
+    public String name;
+    public float length;
+    public int limbs;
+    public String gender;
+    public int eyes;
+
+    public Nematode(String name, float length, int limbs, String gender, int eyes) {
+        this.name = name;
+        this.length = length;
+        this.limbs = limbs;
+        this.gender = gender;
+        this.eyes = eyes;
+    }
+
+    public Nematode(TableRow tr) {
+        this(
+                tr.getString("name"),
+                tr.getFloat("length"),
+                tr.getInt("limbs"),
+                tr.getString("gender"),
+                tr.getInt("eyes"));
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getLength() {
+        return length;
+    }
+
+    public void setLength(float length) {
+        this.length = length;
+    }
+
+    public int getLimbs() {
+        return limbs;
+    }
+
+    public void setLimbs(int limbs) {
+        this.limbs = limbs;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getEyes() {
+        return eyes;
+    }
+
+    public void setEyes(int eyes) {
+        this.eyes = eyes;
+    }
+
+    @Override
+    public String toString() {
+        return "Nematode [eyes=" + eyes + ", gender=" + gender + ", length=" + length + ", limbs=" + limbs + ", name="
+                + name + "]";
+    }   
+
+
+    public static void main() {
+        
+    }
+
+}
--- java/src/ie/tudublin/NematodeVisualiser.java
+++ java/src/ie/tudublin/NematodeVisualiser.java
@@ -6,36 +6,103 @@ import processing.core.PApplet;
 import processing.data.Table;
 import processing.data.TableRow;
 
-public class NematodeVisualiser extends PApplet
-{
+public class NematodeVisualiser extends PApplet {
 
-	public void keyPressed()
-	{		
-		if (keyCode == LEFT)
-		{
-		}		
+	ArrayList<Nematode> NematodeList = new ArrayList<Nematode>();
+
+	int nemaOption = 0;
+
+	public void keyPressed() {
+		if (keyCode == LEFT) {
+
+			if (nemaOption != 0)
+				nemaOption--;
+
+		}
+
+		if (keyCode == RIGHT) {
+			nemaOption++;
+		}
 	}
 
-
-	public void settings()
-	{
+	public void settings() {
 		size(800, 800);
 	}
 
-	public void setup() 
-	{
+	public void setup() {
 		colorMode(HSB);
 		background(0);
-		smooth();				
-	}
-	
-
-	public void loadNematodes()
-	{
+		smooth();
+		loadNematodes();
 	}
 
+	public void loadNematodes() {
 
-	public void draw()
-	{	
+		Table table = loadTable("nematodes.csv", "header");
+		for (TableRow row : table.rows()) {
+			Nematode nema = new Nematode(row);
+			NematodeList.add(nema);
+		}
+	}
+
+	public void draw() {
+
+		int amount = NematodeList.size();
+		float offset = 50;
+
+		String name = NematodeList.get(nemaOption).name;
+		float noCircles = NematodeList.get(nemaOption).length;
+		String gender = NematodeList.get(nemaOption).gender;
+		int limbs = NematodeList.get(nemaOption).limbs;
+		int eyes = NematodeList.get(nemaOption).eyes;
+
+		String male = "m";
+		String female = "f";
+		String herm = "h";
+		
+
+		background(0);
+		textSize(50);
+		textAlign(CENTER);
+		text(name, 400, 50);
+
+		for (int i = 0; i < noCircles; i++) {
+
+			strokeWeight(5);
+			stroke(255);
+			noFill();
+
+			//body length
+			circle(400, (height / 2.5f) + (offset * i), 50);
+
+			//limbs
+			if (limbs == 1) {
+				line(330, 320 + (offset * i), 370, 320 + (offset * i));
+				line(430, 320 + (offset * i), 470, 320 + (offset * i));
+			}
+
+			//genitals
+			if(gender.compareTo(male) == 0 || gender.compareTo(herm) == 0)
+			{
+
+				line(400, 310 + (offset * (noCircles - 1) + 40), 400, 320 + (offset * (noCircles - 1) + 60));
+				circle(400, 320 + (offset * (noCircles - 1) + 70), 10);
+			}
+
+			if(gender.compareTo(female) == 0 || gender.compareTo(herm) == 0)
+			{
+				circle(400, 320 + (offset * (noCircles - 1)), 20);
+			}
+			
+			//eyes
+			if (eyes == 1) {
+				circle(370, 320 - offset, 20);
+				circle(430, 320 - offset, 20);
+				line(370, 330 - offset, 400, 345 - offset);
+				line(430, 330 - offset, 400, 345 - offset);
+			}
+
+		}
+
 	}
 }
